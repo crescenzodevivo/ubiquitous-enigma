93955b6834ec6f3f6da9159295b7f3645743eb9d
crescenzodevivo <crescenzodevivo4@gmail.com>
2026-06-25 09:05:29 +0000

    new file:   .vscode/settings.json 	modified:   lakecomobnb/src/main/java/com/generation/lakecomobnb/model/City.java 	modified:   lakecomobnb/src/main/java/com/generation/lakecomobnb/model/Province.java 	new file:   lakecomobnb/target/classes/com/generation/lakecomobnb/model/City.class 	new file:   lakecomobnb/target/classes/com/generation/lakecomobnb/model/Province.class

--- lakecomobnb/src/main/java/com/generation/lakecomobnb/model/City.java
+++ lakecomobnb/src/main/java/com/generation/lakecomobnb/model/City.java
@@ -4,12 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 
 @Entity
 @Data
 public class City {
-
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private int id;
